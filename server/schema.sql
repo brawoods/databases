7ec32e32c41295c0879a2d268a0096cc52a1d9d8
Brandon Woods <woods.brandon.m@gmail.com>
2023-03-01 18:29:36 -0800

    fix spec tests and start messages.js getAll

--- server/schema.sql
+++ server/schema.sql
@@ -3,29 +3,39 @@ CREATE DATABASE chat;
 USE chat;
 
 /* Create other tables and define schemas for them here! */
-CREATE TABLE rooms (
-  RoomID int NOT NULL,
-  RoomName varchar(255) NOT NULL,
-  PRIMARY KEY (RoomID)
-);
-
-CREATE TABLE users (
-  UserID int NOT NULL,
-  UserName varchar(255) NOT NULL,
-  PRIMARY KEY (UserID)
-);
-
 CREATE TABLE messages (
   /* Describe your table here.*/
   /* id, text, foreign key user, foreign key room*/
   MessageID int NOT NULL,
   TextMessage varchar(255) NOT NULL,
-  User int NOT NULL,
-  Room int NOT NULL,
-  PRIMARY KEY (MessageID),
-  FOREIGN KEY (User) REFERENCES users(UserID),
-  FOREIGN KEY (Room) REFERENCES rooms(RoomID)
+  User varchar(255) NOT NULL,
+  Room varchar(255) NOT NULL,
+  PRIMARY KEY (MessageID)
 );
+
+-- CREATE TABLE rooms (
+--   RoomID int NOT NULL,
+--   RoomName varchar(255) NOT NULL,
+--   PRIMARY KEY (RoomID)
+-- );
+
+-- CREATE TABLE users (
+--   UserID int NOT NULL,
+--   UserName varchar(255) NOT NULL,
+--   PRIMARY KEY (UserID)
+-- );
+
+-- CREATE TABLE messages (
+--   /* Describe your table here.*/
+--   /* id, text, foreign key user, foreign key room*/
+--   MessageID int NOT NULL,
+--   TextMessage varchar(255) NOT NULL,
+--   User int NOT NULL,
+--   Room int NOT NULL,
+--   PRIMARY KEY (MessageID),
+--   FOREIGN KEY (User) REFERENCES users(UserID),
+--   FOREIGN KEY (Room) REFERENCES rooms(RoomID)
+-- );
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
