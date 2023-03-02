907ffed66c38b8c97e31dde2db2422cd3429c9ee
Brandon Woods <woods.brandon.m@gmail.com>
2023-03-01 16:42:51 -0800

    Create database schema file

--- server/schema.sql
+++ server/schema.sql
@@ -2,14 +2,30 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  RoomID int NOT NULL,
+  RoomName varchar(255) NOT NULL,
+  PRIMARY KEY (RoomID)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  UserID int NOT NULL,
+  UserName varchar(255) NOT NULL,
+  PRIMARY KEY (UserID)
+);
 
-
-
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  /* id, text, foreign key user, foreign key room*/
+  MessageID int NOT NULL,
+  TextMessage varchar(255) NOT NULL,
+  User int NOT NULL,
+  Room int NOT NULL,
+  PRIMARY KEY (MessageID),
+  FOREIGN KEY (User) REFERENCES users(UserID),
+  FOREIGN KEY (Room) REFERENCES rooms(RoomID)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
